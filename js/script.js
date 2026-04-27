089fbb5eebc33c04f8c4bff93fa98a3fdfe77789
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-04-27 18:29:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Enhanced Responsive JavaScript for Techsure Solutions
 
+
 // DOM Elements
 const header = document.getElementById('header');
 const hamburger = document.getElementById('hamburger');
