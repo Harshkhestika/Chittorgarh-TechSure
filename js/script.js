ba5c13ed29f93571a9388378f719a0eeb5bc9c2a
Harsh Khestika <151620899+Harshkhestika@users.noreply.github.com>
2026-04-27 18:31:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 // DOM Elements
-const header = document.getElementById('header');
+const header = document.getElementById('header') ;
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeMenu = document.getElementById('close-menu');
